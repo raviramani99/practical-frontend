17853f2d8bcb670c6ea2c8ad43a7832fc227f48b
Ravi Patel <patelravikumar.99@gmail.com>
2024-09-16 09:56:40 +0530

    removed secretes from env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,2 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAQpbJ3RR-t2N9mDIQ0P5z-6SD3f1OMyQU",
-  authDomain: "practical-test-3a971.firebaseapp.com",
-  projectId: "practical-test-3a971",
-  storageBucket: "practical-test-3a971.appspot.com",
-  messagingSenderId: "576632349115",
-  appId: "1:576632349115:web:8d6af2cfcf73959b2fa49e",
-  measurementId: "G-XSG5CGSM28"
 };
